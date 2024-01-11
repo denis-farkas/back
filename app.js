c179aa14ab818ba1c5c523439ce3cd614c616ef0
Denis FARKAS <denis.farkas@laplateforme.io>
2024-01-11 18:28:22 +0100

    ok

--- app.js
+++ app.js
@@ -1,29 +1,44 @@
-require('dotenv').config();
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-require('./models/connection')
+require("dotenv").config();
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+require("./models/connection");
 
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var missionsRouter = require('./routes/missions');
-var cleanersRouter = require('./routes/cleaners');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var missionsRouter = require("./routes/missions");
+var cleanersRouter = require("./routes/cleaners");
 
 var app = express();
-const cors = require('cors');
-app.use(cors());
+const cors = require("cors");
+app.use(
+  cors({
+    origin(origin, callback) {
+      const allowedOrigins = [
+        process.env.FRONTEND_URL,
+        "https://front-xi-wheat.vercel.app/",
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    // origin: process.env.FRONTEND_URL ?? "http://localhost:3000",
+    optionsSuccessStatus: 200,
+    credentials: true,
+  })
+);
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
- app.use('/missions', missionsRouter);
- app.use('/cleaners', cleanersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/missions", missionsRouter);
+app.use("/cleaners", cleanersRouter);
 module.exports = app;
